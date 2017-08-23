3513f30a239c4013dadba0c5a37a66552c32ccc4
Garry Blight <garry.blight@guardian.co.uk>
2017-08-23 12:26:38 +0100

    Initial markup and styles

--- src/render.js
+++ src/render.js
@@ -1,5 +1,23 @@
+import Handlebars from 'handlebars/dist/handlebars'
+import rp from 'request-promise'
 import mainTemplate from './src/templates/main.html!text'
 
-export async function render() {
-    return mainTemplate;
+
+// export async function render() {
+
+//     return mainTemplate;
+
+// }
+
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata/1e0h3eAZRkXJCrh5jN7TIRkp-qkMBa1WjihzdOalpOYE.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        console.log(sheets);
+        //var html = Mustache.render(mainTemplate, sheets, partialTemplates);
+        var html = mainTemplate;
+        return html;
+    });
 }
