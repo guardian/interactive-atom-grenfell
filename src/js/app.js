11235c4b38e47ecc487681a2304bf4a5c26b2e11
Garry Blight <garry.blight@guardian.co.uk>
2017-09-06 15:17:31 +0100

    added throttle to scroll to alleviate flicker in info box

--- src/js/app.js
+++ src/js/app.js
@@ -152,12 +152,17 @@ function updateViewAfterResize() {
 }
 
 function updateViewAfterScroll(){
+   
+    throttle(updateViewAfterScrollDeferred(), 200);
+}
+
+function updateViewAfterScrollDeferred(){
     checkFixView();
     let lvl = getLevelFromScroll(globalLevel);
 
     updateLevelView(lvl);
     updateInfoBox(lvl);
-    console.log("test");
+    
     console.log("globalLevel after scroll",globalLevel);
 
     document.querySelector("#gv-navs").classList.remove("gv-hide"); // ADDED
@@ -352,6 +357,10 @@ function getLevelFromScroll(n) {
     //     n = 0;
     // }
 
+    if (n < -1) {
+        n=-1;
+    }
+
     console.log("n=" + n);
     console.log("g=" + globalLevel);
 
@@ -532,4 +541,41 @@ function addThumbGallery(dataIn) {
     var newHTML = content(dataIn);
     return newHTML
 }
+
+// Returns a function, that, when invoked, will only be triggered at most once
+// during a given window of time. Normally, the throttled function will run
+// as much as it can, without ever going more than once per `wait` duration;
+// but if you'd like to disable the execution on the leading edge, pass
+// `{leading: false}`. To disable execution on the trailing edge, ditto.
+function throttle(func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function() {
+      previous = options.leading === false ? 0 : Date.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+    return function() {
+      var now = Date.now();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  };
 
